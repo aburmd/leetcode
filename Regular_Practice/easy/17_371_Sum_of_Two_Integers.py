416c8e49cfda421e44fe40929df83d7c941dc14b
Abuhuraira Mohamed Ibrahim <abuhura@amazon.com>
2019-12-28 17:13:34 -0800

    Second Commit: Same approach little claen code

--- Regular_Practice/easy/17_371_Sum_of_Two_Integers.py
+++ Regular_Practice/easy/17_371_Sum_of_Two_Integers.py
@@ -12,41 +12,44 @@ Output: 1
 """
 
 class Solution:
-    def getPositiveIntegerSum(self,a,b):
+    def getPositivesum(self,a,b):
         while b!=0:
-            bitcommon=a&b
-            a=a^b #bitdiff assign back to a
-            b=bitcommon<<1 #push one bit to adjust the value
+            bitcommon=a&b #Example: 4(100),5(101) bit common(a&b) is 4(100)
+            a=a^b #Example: 4(100),5(101) bit diff(a^b) is 1(001)
+            b=bitcommon<<1 #Example: one shift, 4 one shift (4(100)<<1) is 8(1000)
         return a
-
-    def getNegativeIntegerSum(self,a,b):
-        upperbound=0xfffffffff #define the max value for calculation
-        upperbound_plus1=self.getPositiveIntegerSum(upperbound,1)
-        b=b&0xfffffffff #boundary define for negative number and negative number starts in reverse order like (-1&0xf->15,-1&0xff->255) here -1&0xfffffffff = -1=68719476735)
-        a=self.getPositiveIntegerSum(a,b)
-        if a>=upperbound: # above upperbound value can be controlled using mod function
-            return -1 if a%upperbound==0 else a%upperbound_plus1
+    def getNegativesum(self,apos,bneg):
+        upperbound=0xfffffffff #f-1111 ie 0xf-15 define the max value for calculation
+        upperbound_plus1=self.getPositivesum(upperbound,1)
+        b=bneg&0xfffffffff #negative value starts in reverse order from upperbound
+        #like (-1&0xf->15,-1&0xff->255) here -1&0xfffffffff = -1=68719476735)
+        a=self.getPositivesum(apos,b)
+        if a==upperbound:
+            return -1
+        elif a>upperbound:
+            return a%upperbound_plus1
         else:
-            return -1*((-1*(a%upperbound))&0xfffffffff) # below upperbound value can be controlled by make negative define upperbound and multiply by -1
-
+            return -1*((-1*a)&0xfffffffff)
     def getSum(self,a, b):
-        if a==-b:
+        if a==(-1)*b:
             return 0
-        if a>=0 and b>=0:
-            return self.getPositiveIntegerSum(a,b)
+        elif a>=0 and b>=0:
+            return self.getPositivesum(a,b)
+        elif a<=0 and b<=0:
+            apos=-1*a
+            bpos=-1*b
+            return -1*self.getPositivesum(apos,bpos)
         else:
-            if b<=0 and a<=0:
-                b=-1*b
-                a=-1*a
-                x=self.getSum(a,b)
-                return -1*x
-            elif b<0:
-                return self.getNegativeIntegerSum(a,b)
-            elif a<0:
-                return self.getNegativeIntegerSum(b,a)
+            if b<0:
+                return self.getNegativesum(a,b)
+            else:
+                return self.getNegativesum(b,a)
 
 
 """
+SecondCommit:
+Runtime: 24 ms, faster than 91.03% of Python3 online submissions for Sum of Two Integers.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Sum of Two Integers..
 FirstCommit:
 Runtime: 24 ms, faster than 92.83% of Python3 online submissions for Sum of Two Integers.
 Memory Usage: 12.7 MB, less than 100.00% of Python3 online submissions for Sum of Two Integers.
