9dc740d49bf1a600c23f0dc689d41c4b0048eef9
Abuhuraira Mohamed Ibrahim <abuhura@amazon.com>
2019-12-28 17:54:24 -0800

    Second Commit: Using Counter module, 16_387_First_Unique_Character_in_a_String.py much faster

--- Regular_Practice/easy/16_387_First_Unique_Character_in_a_String.py
+++ Regular_Practice/easy/16_387_First_Unique_Character_in_a_String.py
@@ -12,27 +12,20 @@ Note: You may assume the string contain only lowercase letters.
 """
 
 
+import collections
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        if len(s)==0:
-            return -1
-        elif len(s)==1:
-            return 0
-        dct={}
-        lst=[]
-        for key,value in enumerate(s):
-            if value not in dct.keys():
-                dct[value]=key
-            else:
-                dct[value]=-1
-        for value,key in list(dct.items()):
-            if (key!=-1) and (key not in lst):
-                lst.append(key)
-        lst.sort()
-        return -1 if not len(lst) else lst[0]
+        c=collections.Counter(s)
+        for i in range(0,len(s)):
+            if c[s[i]]==1:
+                return i
+        return -1
 
 
 """
+SecondCommit:
+Runtime: 88 ms, faster than 88.45% of Python3 online submissions for First Unique Character in a String.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for First Unique Character in a String.
 FirstCommit:
 Runtime: 144 ms, faster than 33.70% of Python3 online submissions for First Unique Character in a String.
 Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for First Unique Character in a String.
